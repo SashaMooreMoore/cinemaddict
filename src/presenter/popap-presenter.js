4f38cf1143499f65feaa72cfc5fac278667ab674
Sasha Moore <sansopanso@gmail.com>
2025-12-18 15:31:44 +0300

    Закончил верхнюю панель film-details-top-container. Кнопки секции film-details-controls-section не прорисовались. Возможно, потому что нужно дорисовать нижнюю панель.

--- src/presenter/popap-presenter.js
+++ src/presenter/popap-presenter.js
@@ -3,6 +3,11 @@ import FilmDetailsSection from '../view/film-details-section';
 import FilmDetailsForm from '../view/film-details-form';
 import FilmDetailsTopContainer from '../view/film-details-top-container';
 import FilmDetailsBottomContainer from '../view/film-details-bottom-container';
+import FilmDetailsCloseBtn from '../view/film-details-close-btn';
+import FilmDetailsInfoWrap from '../view/film-details-info-wrap';
+import FilmDetailsPosterImg from '../view/film-details-poster';
+import FilmDetailsInfo from '../view/film-details-info';
+import FilmDetailsControlsSection from '../view/film-details-controls-section';
 
 
 export default class PopapPresenter {
@@ -10,6 +15,11 @@ export default class PopapPresenter {
   filmDetailsForm = new FilmDetailsForm();
   filmDetailsTopContainer = new FilmDetailsTopContainer();
   filmDetailsBottomContainer = new FilmDetailsBottomContainer();
+  filmDetailsCloseBtn = new FilmDetailsCloseBtn();
+  filmDetailsInfoWrap = new FilmDetailsInfoWrap();
+  filmDetailsPosterImg = new FilmDetailsPosterImg();
+  filmDetailsInfo = new FilmDetailsInfo();
+  filmDetailsControlsSection = new FilmDetailsControlsSection();
 
   init = (popapContainer) => {
     this.popapContainer = popapContainer;
@@ -17,5 +27,11 @@ export default class PopapPresenter {
     render(this.filmDetailsForm, this.filmDetailsSection.getElement());
     render(this.filmDetailsTopContainer, this.filmDetailsForm.getElement());
     render(this.filmDetailsBottomContainer, this.filmDetailsForm.getElement());
+    render(this.filmDetailsCloseBtn, this.filmDetailsTopContainer.getElement());
+    render(this.filmDetailsInfoWrap, this.filmDetailsTopContainer.getElement());
+    render(this.filmDetailsPosterImg, this.filmDetailsInfoWrap.getElement());
+    render(this.filmDetailsInfo, this.filmDetailsInfoWrap.getElement());
+    render(this.filmDetailsControlsSection, this.filmDetailsInfoWrap.getElement());
+
   };
 }
---
+++ src/view/film-details-poster.js
@@ -0,0 +1,25 @@
+import { createElement } from '../render.js';
+
+const createFilmDetailsPosterTemplate = () =>
+  `<div class="film-details__poster">
+          <img class="film-details__poster-img" src="./images/posters/the-great-flamarion.jpg" alt="">
+
+          <p class="film-details__age">18+</p>
+        </div>`;
+
+export default class FilmDetailsPosterImg {
+  getTemplate() {
+    return createFilmDetailsPosterTemplate();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
