16f25a17b5a59f9fe734893a2ad8f666992dfa63
Sasha Moore <sansopanso@gmail.com>
2026-01-09 12:18:21 +0300

    Переписал попаппрезентер, теперь передаю попап не по кусочкам, а одним куском, для этого завел файл popap.js

--- src/presenter/popap-presenter.js
+++ src/presenter/popap-presenter.js
@@ -1,50 +1,53 @@
 import { render, RenderPosition } from '../render';
-import FilmDetailsSection from '../view/film-details-section';
-import FilmDetailsForm from '../view/film-details-form';
-import FilmDetailsTopContainer from '../view/film-details-top-container';
-import FilmDetailsBottomContainer from '../view/film-details-bottom-container';
-import FilmDetailsCloseBtn from '../view/film-details-close-btn';
-import FilmDetailsInfoWrap from '../view/film-details-info-wrap';
-import FilmDetailsPosterImg from '../view/film-details-poster';
-import FilmDetailsInfo from '../view/film-details-info';
-import FilmDetailsControlsSection from '../view/film-details-controls-section';
-import FilmDetailsCommentsWrap from '../view/film-details-comments-wrap';
-import FilmDetailsCommentsTitle from '../view/film-details-comments-title';
-import FilmDetailsCommentsList from '../view/film-details-comments-list';
-import FilmDetailsNewComment from '../view/film-details-new-comment';
+// import FilmDetailsSection from '../view/film-details-section';
+// import FilmDetailsForm from '../view/film-details-form';
+// import FilmDetailsTopContainer from '../view/film-details-top-container';
+// import FilmDetailsBottomContainer from '../view/film-details-bottom-container';
+// import FilmDetailsCloseBtn from '../view/film-details-close-btn';
+// import FilmDetailsInfoWrap from '../view/film-details-info-wrap';
+// import FilmDetailsPosterImg from '../view/film-details-poster';
+// import FilmDetailsInfo from '../view/film-details-info';
+// import FilmDetailsControlsSection from '../view/film-details-controls-section';
+// import FilmDetailsCommentsWrap from '../view/film-details-comments-wrap';
+// import FilmDetailsCommentsTitle from '../view/film-details-comments-title';
+// import FilmDetailsCommentsList from '../view/film-details-comments-list';
+// import FilmDetailsNewComment from '../view/film-details-new-comment';
+import PopapView from '../view/popap';
 
 
 export default class PopapPresenter {
-  filmDetailsSection = new FilmDetailsSection();
-  filmDetailsForm = new FilmDetailsForm();
-  filmDetailsTopContainer = new FilmDetailsTopContainer();
-  filmDetailsBottomContainer = new FilmDetailsBottomContainer();
-  filmDetailsCloseBtn = new FilmDetailsCloseBtn();
-  filmDetailsInfoWrap = new FilmDetailsInfoWrap();
-  filmDetailsPosterImg = new FilmDetailsPosterImg();
-  filmDetailsInfo = new FilmDetailsInfo();
-  filmDetailsControlsSection = new FilmDetailsControlsSection();
-  filmDetailsCommentsWrap = new FilmDetailsCommentsWrap();
-  filmDetailsCommentsTitle = new FilmDetailsCommentsTitle();
-  filmDetailsCommentsList = new FilmDetailsCommentsList();
-  filmDetailsNewComment = new FilmDetailsNewComment();
+  // filmDetailsSection = new FilmDetailsSection();
+  // filmDetailsForm = new FilmDetailsForm();
+  // filmDetailsTopContainer = new FilmDetailsTopContainer();
+  // filmDetailsBottomContainer = new FilmDetailsBottomContainer();
+  // filmDetailsCloseBtn = new FilmDetailsCloseBtn();
+  // filmDetailsInfoWrap = new FilmDetailsInfoWrap();
+  // filmDetailsPosterImg = new FilmDetailsPosterImg();
+  // filmDetailsInfo = new FilmDetailsInfo();
+  // filmDetailsControlsSection = new FilmDetailsControlsSection();
+  // filmDetailsCommentsWrap = new FilmDetailsCommentsWrap();
+  // filmDetailsCommentsTitle = new FilmDetailsCommentsTitle();
+  // filmDetailsCommentsList = new FilmDetailsCommentsList();
+  // filmDetailsNewComment = new FilmDetailsNewComment();
 
   init = (popapContainer, filmModel) => {
     this.popapContainer = popapContainer;
     this.filmModel = filmModel;
 
-    render(this.filmDetailsSection, popapContainer, RenderPosition.AFTEREND);
-    render(this.filmDetailsForm, this.filmDetailsSection.getElement());
-    render(this.filmDetailsTopContainer, this.filmDetailsForm.getElement());
-    render(this.filmDetailsBottomContainer, this.filmDetailsForm.getElement());
-    render(this.filmDetailsCloseBtn, this.filmDetailsTopContainer.getElement());
-    render(this.filmDetailsInfoWrap, this.filmDetailsTopContainer.getElement());
-    render(this.filmDetailsPosterImg, this.filmDetailsInfoWrap.getElement());
-    render(this.filmDetailsInfo, this.filmDetailsInfoWrap.getElement());
-    render(this.filmDetailsControlsSection, this.filmDetailsTopContainer.getElement());
-    render(this.filmDetailsCommentsWrap, this.filmDetailsBottomContainer.getElement());
-    render(this.filmDetailsCommentsTitle, this.filmDetailsCommentsWrap.getElement());
-    render(this.filmDetailsCommentsList, this.filmDetailsCommentsWrap.getElement());
-    render(this.filmDetailsNewComment, this.filmDetailsCommentsWrap.getElement());
+    render(new PopapView(this.filmModel), this.popapContainer, RenderPosition.AFTEREND);
+
+    // render(this.filmDetailsSection, popapContainer, RenderPosition.AFTEREND);
+    // render(this.filmDetailsForm, this.filmDetailsSection.getElement());
+    // render(this.filmDetailsTopContainer, this.filmDetailsForm.getElement());
+    // render(this.filmDetailsBottomContainer, this.filmDetailsForm.getElement());
+    // render(this.filmDetailsCloseBtn, this.filmDetailsTopContainer.getElement());
+    // render(this.filmDetailsInfoWrap, this.filmDetailsTopContainer.getElement());
+    // render(this.filmDetailsPosterImg, this.filmDetailsInfoWrap.getElement());
+    // render(this.filmDetailsInfo, this.filmDetailsInfoWrap.getElement());
+    // render(this.filmDetailsControlsSection, this.filmDetailsTopContainer.getElement());
+    // render(this.filmDetailsCommentsWrap, this.filmDetailsBottomContainer.getElement());
+    // render(this.filmDetailsCommentsTitle, this.filmDetailsCommentsWrap.getElement());
+    // render(this.filmDetailsCommentsList, this.filmDetailsCommentsWrap.getElement());
+    // render(this.filmDetailsNewComment, this.filmDetailsCommentsWrap.getElement());
   };
 }
