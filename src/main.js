ccda2c6fd45d885d7cc7a76ef12a83ebbaf14cff
Sasha Moore <sansopanso@gmail.com>
2026-01-22 18:48:34 +0300

    Поскольку ранее перенес отрисовку футера в #renderBoardCards, то почистил файл main.js, удалил из него строки касательно футера

--- src/main.js
+++ src/main.js
@@ -1,19 +1,16 @@
 import UserRankView from './view/user-rank.js';
-import FooterFilmsStatisticsView from './view/footer-statistics-view.js';
 import MenuView from './view/menu.js';
 import FilmsPresenter from './presenter/films-presenter.js';
 import { render, RenderPosition } from './render.js';
 import MovieModel from './model/movie-model.js';
 
 const siteHeaderElement = document.querySelector('.header');
-const footerStatisticsElement = document.querySelector('.footer__statistics');
 const siteMainElement = document.querySelector('.main');
 
 const filmsPresenter = new FilmsPresenter();
 const movieModel = new MovieModel();
 
 render(new UserRankView(), siteHeaderElement);
-// render(new FooterFilmsStatisticsView(), footerStatisticsElement);
 render(new MenuView(), siteMainElement, RenderPosition.AFTERBEGIN);
 
 filmsPresenter.init(siteMainElement, movieModel);
